e70c3348d68cdb02d4c2d15424e8de1be72dc683
Mohamed Nufail <eg173254@engug.ruh.ac.lk>
2022-07-27 16:28:52 +0530

    Added Weather Entity

---
+++ src/main/java/com/weather/system/entity/Weather.java
@@ -0,0 +1,33 @@
+package com.weather.system.entity;
+
+
+import lombok.*;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mapping.KPropertyPathExtensionsKt;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Data
+@Document
+@Getter
+@Setter
+@NoArgsConstructor
+@EnableMongoRepositories
+@ComponentScan
+public class Weather {
+
+    @Id
+    private String id;
+    private String location;
+    private String wind_speed;
+    private String pressure;
+    private String humidity;
+
+    public Weather(String location, String wind_speed, String pressure, String humidity) {
+        this.location = location;
+        this.wind_speed = wind_speed;
+        this.pressure = pressure;
+        this.humidity = humidity;
+    }
+}
